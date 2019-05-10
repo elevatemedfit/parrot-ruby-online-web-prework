a167d0352417bcde80a91c70112f588cfe575604
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-10 18:15:20 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
-def Parrot(parrot_talk)
-  parrot_talk = "Polly want a cracker!"
+def Parrot(parrot_talk = "Squawk")
   puts parrot_talk
   return parrot_talk
 end
 # returns the phrase
-parrot
+parrot()
